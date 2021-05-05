8d0d8ffca2bb80ea698d0e572fb717499cfa3f51
Udit Bhati <55918713+udit19281@users.noreply.github.com>
2021-05-06 00:06:35 +0530

    update

--- main/models.py
+++ main/models.py
@@ -4,16 +4,20 @@ from django.db import models
 
 class Project(models.Model):
     choi=[
-        ('cabin','cabin'),
-        ('cake','cake'),
-        ('circus','circus'),
-        ('game','game'),
-        ('safe','safe'),
-        ('submarine','submarine')
+        ('1','cabin'),
+        ('2','cake'),
+        ('6','circus'),
+        ('5','game'),
+        ('4','safe'),
+        ('3','submarine'),
+        ('7','linux'),
+        ('8','color switch'),
+        ('9','python'),
+        ('10','smart shoe')
     ]
     name = models.CharField(max_length=50)
     details=models.CharField(max_length=200)
-    image=models.CharField(max_length=10,choices=choi)
+    image=models.CharField(max_length=20,choices=choi)
 
     def __str__(self):
         return self.name
