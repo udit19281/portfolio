8c87608436b2f36c27c95297a736fc41c8e2ae04
udit19281 <udit19281@iiitd.ac.in>
2021-06-03 00:05:46 +0530

    update

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=50)
     details=models.CharField(max_length=200)
-    image=models.URLField(max_length=200,default='http://www.foo.com')
+    image=models.URLField(max_length=500,default='http://www.foo.com')
     link=models.URLField(max_length=200)
 
     def __str__(self):
